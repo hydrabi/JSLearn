fe80d81a44997be5779bf6b6537dbada0627b1f3
hydrabi <bizhifengted@gmail.com>
2019-08-30 21:46:12 +0800

    临时提交

--- Dom/第十章/common.js
+++ Dom/第十章/common.js
@@ -22,6 +22,39 @@ function getNextElement(node) {
     return null;
 }
 
+function moveElement(elementId, final_x, final_y, interval) {
+    if (!document.getElementById) return false
+    if (!document.getElementById(elementId)) return false
+    var elem = document.getElementById(elementId)
+    var xpos = parseInt(elem.style.left)
+    var ypos = parseInt(elem.style.top)
+
+    if (xpos == final_x && ypos == final_y) {
+        return true
+    }
+
+    if (xpos < final_x) {
+        xpos++;
+    }
+
+    if (xpos > final_x) {
+        xpos--;
+    }
+
+    if (ypos < final_y) {
+        ypos++;
+    }
+
+    if (ypos > final_y) {
+        ypos--;
+    }
+
+    elem.style.left = xpos + "px"
+    elem.style.top = ypos + "px"
+    var repeat = "moveElement('" + elementId + "'," + final_x + "," + final_y + "," + interval + ")"
+    movement = setTimeout(repeat, interval)
+}
+
 function addClass(element, value) {
     if (!element.className) {
         element.className = value
@@ -40,14 +73,39 @@ function positionMessage() {
     elem.style.position = "absolute"
     elem.style.left = "50px"
     elem.style.top = "100px"
-    movement = setTimeout("moveMessage()", 5000)
+    movement = setTimeout("moveMessage()", 1000)
 }
 
 function moveMessage() {
     if (!document.getElementById) return false
     if (!document.getElementById("message")) return false
     var elem = document.getElementById("message")
-    elem.style.left = "200px"
+    var xpos = parseInt(elem.style.left)
+    var ypos = parseInt(elem.style.top)
+
+    if (xpos == 200 && ypos == 100) {
+        return true
+    }
+
+    if (xpos < 200) {
+        xpos++;
+    }
+
+    if (xpos > 200) {
+        xpos--;
+    }
+
+    if (ypos < 100) {
+        ypos++;
+    }
+
+    if (ypos > 100) {
+        ypos--;
+    }
+
+    elem.style.left = xpos + "px"
+    elem.style.top = ypos + "px"
+    movement = setTimeout("moveMessage()", 10)
 }
 
 addloadEvent(positionMessage)
